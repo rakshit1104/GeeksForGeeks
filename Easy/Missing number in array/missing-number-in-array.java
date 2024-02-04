a41e26a8e3f2bfd20c828054714d227a30b9fcd1
Rakshit Pandey <rakshit.lucky@gmail.com>
2024-02-04 16:09:40 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Missing number in array/missing-number-in-array.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().trim().split(" ");
+            int[] array = new int[n - 1];
+            for (int i = 0; i < n - 1; i++) {
+                array[i] = Integer.parseInt(str[i]);
+            }
+            Solution sln = new Solution();
+            System.out.println(sln.missingNumber(array, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int missingNumber(int array[], int n) {
+        // Your Code Here
+        int sum_expected = 0;
+        int sum = 0;
+        
+        for(int i = 1; i <= n; i++){
+            sum_expected = sum_expected + i;
+        }
+        
+        for(int j : array){
+            sum = sum + j;
+        }
+        
+        return (sum_expected - sum);
+    }
+}
