ca53c614ecb8d25c28aaea170bf4e6e7ceb573bf
Rakshit Pandey <rakshit.lucky@gmail.com>
2024-02-08 23:36:34 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Anagram/anagram.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.util.stream.*; 
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    
+	public static void main (String[] args)throws IOException {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		while(t-->0)
+		{
+		    String str = br.readLine();
+		    String s1 = str.split(" ")[0];
+		    String s2 = str.split(" ")[1];
+		    
+		    Solution obj = new Solution();
+		    
+		    if(obj.isAnagram(s1,s2))
+		    {
+		        System.out.println("YES");
+		    }
+		    else
+		    {
+		         System.out.println("NO");
+		    }
+		    
+		    
+		    
+		}
+	}
+}
+// } Driver Code Ends
+
+
+class Solution
+{    
+    //Function is to check whether two strings are anagram of each other or not.
+    public static boolean isAnagram(String a,String b)
+    {
+        
+        // Your code here
+        if(a.length() != b.length()){
+            return false;
+        }
+        
+        HashMap<Character,Integer> map=new HashMap<Character,Integer>();
+        
+        for(int i=0; i<a.length(); i++){
+            map.put(a.charAt(i),map.getOrDefault(a.charAt(i),0)+1);
+        }
+        
+        for(int i=0; i<b.length(); i++){
+            if(map.containsKey(b.charAt(i)) && map.get(b.charAt(i))>0){
+                map.put(b.charAt(i),map.get(b.charAt(i))-1);
+            }
+        }
+        
+        for(char e:map.keySet()){
+            if(map.get(e)>0)
+            return false;
+        }
+        
+        return true;
+    }
+}
