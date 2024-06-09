6aff7b7aef8cff63c33529d41e96740c6761166b
Rakshit Pandey <rakshit.lucky@gmail.com>
2024-06-09 22:49:22 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Triplet Sum in Array/triplet-sum-in-array.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main
+{
+    public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    int n = Integer.parseInt(inputLine[0]);
+		    int X = Integer.parseInt(inputLine[1]);
+		    int A[] = new int[n];
+		    inputLine = br.readLine().trim().split(" ");
+		    for(int i=0; i<n; i++){
+		        A[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    Solution ob=new Solution();
+		    boolean ans = ob.find3Numbers(A, n, X);
+		    System.out.println(ans?1:0);
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find if there exists a triplet in the 
+    //array A[] which sums up to X.
+    public static boolean find3Numbers(int nums[], int n, int X) { 
+    
+        // Your code Here
+        Arrays.sort(nums);
+        boolean ans = false;
+        
+        for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            
+            int j = i+1;
+            int k = n-1;
+            
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum < X){
+                    j++;
+                } else if(sum > X){
+                    k--;
+                } else{
+                    if(sum == X) ans = true;
+                    j++;
+                    k--;
+                    while(j < k && nums[j] == nums[j-1]) j++;
+                    while(j < k && nums[k] == nums[k+1]) k--;
+                }
+            }
+        }
+        return ans;
+    }
+}
