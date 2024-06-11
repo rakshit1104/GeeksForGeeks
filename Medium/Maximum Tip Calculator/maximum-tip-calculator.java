20901ee947ff8e57aa3e544c50133bbeb6954222
Rakshit Pandey <rakshit.lucky@gmail.com>
2024-06-11 10:30:47 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximum Tip Calculator/maximum-tip-calculator.java
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int n;
+            n = Integer.parseInt(br.readLine());
+
+            int x;
+            x = Integer.parseInt(br.readLine());
+
+            int y;
+            y = Integer.parseInt(br.readLine());
+
+            int[] arr = IntArray.input(br, n);
+
+            int[] brr = IntArray.input(br, n);
+
+            Solution obj = new Solution();
+            long res = obj.maxTip(n, x, y, arr, brr);
+
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static long maxTip(int n, int x, int y, int[] arr, int[] brr) {
+        // code here
+        Integer[] indices = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            indices[i] = i;
+        }
+        Arrays.sort(indices, (i, j) -> Math.abs(brr[j] - arr[j]) - Math.abs(brr[i] - arr[i]));
+        
+        long ans = 0;
+        for (int i : indices) {
+            if ((arr[i] > brr[i] && x > 0) || y == 0) {
+                ans += arr[i];
+                x--;
+            } else {
+                ans += brr[i];
+                y--;
+            }
+        }
+        
+        return ans;
+    }
+}
