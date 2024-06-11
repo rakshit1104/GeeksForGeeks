43c42737a11c5a8d3c6b1ffaa7eefa632977ca1c
Rakshit Pandey <rakshit.lucky@gmail.com>
2024-06-11 14:42:15 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Rotation/rotation.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+
+            int ans = new Solution().findKRotation(a, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+class Solution {
+    int findKRotation(int arr[], int n) {
+        // code here
+        int low = 0, high = arr.length - 1;
+        int ans = Integer.MAX_VALUE;
+        int index = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[low] <= arr[high]) {
+                if (arr[low] < ans) {
+                    index = low;
+                    ans = arr[low];
+                }
+                break;
+            }
+
+            if (arr[low] <= arr[mid]) {
+                if (arr[low] < ans) {
+                    index = low;
+                    ans = arr[low];
+                }
+                low = mid + 1;
+            } else {
+                if (arr[mid] < ans) {
+                    index = mid;
+                    ans = arr[mid];
+                }
+                high = mid - 1;
+            }
+        }
+        return index;
+    }
+}
