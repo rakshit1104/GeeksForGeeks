323f5d6dc4f4c53328e2c6cb3797270e523f1881
Rakshit Pandey <rakshit.lucky@gmail.com>
2024-07-04 15:11:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Introduction to Linked List/introduction-to-linked-list.java
+++ Difficulty: Easy/Introduction to Linked List/introduction-to-linked-list.java
@@ -51,13 +51,15 @@ class Node {
 class Solution {
     static Node constructLL(int arr[]) {
         // code here
-        Node head = new Node(arr[0]);
-        Node temp = head;
-        for(int i = 1; i<arr.length; i++){
-            Node temp1 = new Node(arr[i]);
-            temp.next = temp1;
-            temp = temp1;
-        }
-        return head;
+       Node head = new Node(arr[0]);
+       Node mover = head;
+       
+       for(int i=1; i<arr.length; i++){
+           Node temp = new Node(arr[i]);
+           mover.next = temp;
+           mover = temp;
+       }
+       
+       return head;
     }
 }
