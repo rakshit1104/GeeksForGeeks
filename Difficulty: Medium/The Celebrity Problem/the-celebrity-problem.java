d85d548fc7576fac17e172e790a914cc908c6493
Rakshit Pandey <rakshit.lucky@gmail.com>
2024-08-03 21:20:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.java
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.java
@@ -1,62 +1,40 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
 import java.io.*;
-import java.util.*; 
+import java.util.*;
 
-class GFG{
-    public static void main(String args[]) throws IOException { 
+class GFG {
+    public static void main(String args[]) throws IOException {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        while(t>0)
-        {
+        while (t > 0) {
             int N = sc.nextInt();
             int M[][] = new int[N][N];
-            for(int i=0; i<N; i++)
-            {
-                for(int j=0; j<N; j++)
-                {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
                     M[i][j] = sc.nextInt();
                 }
             }
-            System.out.println(new Solution().celebrity(M,N));
+            System.out.println(new Solution().celebrity(M));
             t--;
         }
-    } 
-} 
+    }
+}
 // } Driver Code Ends
 
-
-//User function Template for Java
-
-
-class Solution
-{ 
-    //Function to find if there is a celebrity in the party or not.
-    int celebrity(int matrix[][], int n)
-    {
-    	// code here
-    	int top =  0;
-    	int down = n-1;
-    	
-    	while(top<down){
-    	    if(matrix[top][down] == 1){
-    	        top = top+1;
-    	    } else{
-    	        down--;
-    	    }
-    	}
-    	
-    	if(top>down) return -1;
-    	
-    	for(int i = 0; i<n; i++){
-    	    if(i == top) continue;
-    	    if(matrix[top][i] == 0 && matrix[i][top] == 1){
-    	        continue;
-    	    }else{
-    	        return -1;
-    	    }
-    	}
-    	return top;
+class Solution {
+    public int celebrity(int[][] mat) {
+        int n = mat.length, j = 0;
+        for(int i=0;i<n;i++){
+            if(knows(mat, j , i)) j = i;
+        }
+        for(int i=0;i<n;i++){
+            if(j != i && (knows(mat, j, i) || !knows(mat, i, j))) return -1;
+        }
+        return j;
+    }
+    private boolean knows(int[][] m, int a, int b){
+        return m[a][b] == 1;
     }
 }
