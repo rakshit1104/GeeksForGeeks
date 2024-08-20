8e3bc1ea15f2e20dfe7da2f0855269d5f1d429ab
Rakshit Pandey <rakshit.lucky@gmail.com>
2024-08-20 11:51:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Burning Tree/burning-tree.java
+++ Difficulty: Hard/Burning Tree/burning-tree.java
@@ -110,47 +110,63 @@ class GfG {
 
 // } Driver Code Ends
 
-class Solution {
-    int max = 0;
-
-    public int minTime(Node root, int target) {
-        Stack<Node> st = new Stack<>();
-        find(root, st, target);
-
-        Node parent = null;
-        int j=0;
-        while (!st.isEmpty()) {
-            Node current = st.pop();
-            distance(current, j++, parent);
-            parent = current;
-        }
-
-        return max;
+class Solution
+{
+    /*class Node {
+    	int data;
+    	Node left;
+    	Node right;
+    
+    	Node(int data) {
+    		this.data = data;
+    		left = null;
+    		right = null;
+    	}
+    }*/
+    static int timeTaken;
+    public static int minTime(Node root, int target) 
+    {
+        timeTaken=0;
+        solve(root,target);
+        return timeTaken;
     }
-
-    void distance(Node root, int k, Node parent) {
-        if (root == null) {
-            max = Math.max(k - 1, max);
-            return;
+    
+    static Info solve(Node root,int target){
+        if(root==null)
+        return new Info(false,-1);
+        Info left=solve(root.left,target);
+        Info right=solve(root.right,target);
+        
+        int max=1+Math.max(left.height,right.height);
+        //timeTaken=Math.max(timeTaken,max);
+        if(root.data==target){
+            // check and update height with children
+            timeTaken=Math.max(timeTaken,max);
+            right.fireSet=true;
+            right.height=0;
+            return right;
         }
-
-        if (root.left != parent)
-            distance(root.left, k + 1, root);
-        if (root.right != parent)
-            distance(root.right, k + 1, root);
+        if(left.fireSet){
+            timeTaken=Math.max(timeTaken,left.height+right.height+2);
+            left.height+=1;
+            return left;
+        }
+        if(right.fireSet){
+            timeTaken=Math.max(timeTaken,left.height+right.height+2);
+            right.height+=1;
+            return right;
+        }
+        left.height=max;
+        return left;
     }
-
-    boolean find(Node root, Stack<Node> st, int target) {
-        if (root == null)
-            return false;
-        if (root.data == target) {
-            st.push(root);
-            return true;
+    
+    
+    static class Info{
+        boolean fireSet;
+        int height;
+        Info(boolean fireSet,int height){
+            this.fireSet=fireSet;
+            this.height=height;
         }
-        st.push(root);
-        if (find(root.left, st, target) || find(root.right, st, target))
-            return true;
-        st.pop();
-        return false;
     }
 }
