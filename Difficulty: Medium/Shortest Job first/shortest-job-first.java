a460457d3ac06ae2fbf8a47a86abe20bbb1e62ef
Rakshit Pandey <rakshit.lucky@gmail.com>
2024-08-22 12:49:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Shortest Job first/shortest-job-first.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+  public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    int T = sc.nextInt();
+    while (T-- > 0) {
+      int n = sc.nextInt();
+      int a[] = new int[n];
+      for(int i=0;i<n;i++){
+        a[i]=sc.nextInt();
+      }
+      Solution obj = new Solution();
+      int ans = obj.solve(a);
+      System.out.println(ans);
+    }
+  }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int solve(int bt[] ) {
+    // code here
+    int time = 0;
+    int wait_time = 0;
+    int n = bt.length;
+    Arrays.sort(bt);
+    
+    for(int i = 0; i<n; i++){
+        wait_time += time;
+        time += bt[i];
+    }
+    return(wait_time/n);
+  }
+}
+     
